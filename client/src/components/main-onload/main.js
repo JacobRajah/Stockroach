1ac334bfa164a1de0bf8da4c4eda0954ef45526d
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-01-15 21:03:23 -0500

    initial UI commit

---
+++ client/src/components/main-onload/main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './main.css';
+// import axios from 'axios'
+
+class Main extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Welcome to Stockroach</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
--- client/src/react-builder/App.js
+++ client/src/react-builder/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Customers from '../components/testing/customers'
+import Main from '../components/main-onload/main'
 
 function App() {
   return (
     <div className="App">
-      <Customers></Customers>
+      <Main></Main>
     </div>
   );
 }
