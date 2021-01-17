50e973f92718a58894582f0b31442306302ca662
KristinHuang <kristinhuangg@gmail.com>
2021-01-16 20:38:33 -0800

    search page

--- client/src/components/search-page/searchPage.js
+++ client/src/components/search-page/searchPage.js
@@ -1,17 +1,79 @@
 import React, {Component} from 'react'
 // import './main.css';
 import NavBar from '../templates/navbar';
+import Graph from '../graphing/graph';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import useStyles from '../templates/Dashboard';
+import './searchPage.css';
 
-class SearchPage extends Component { 
 
-    render() {
-        return(
-            <div>
-                <NavBar></NavBar>
-                <h1>SearchPage</h1>
+  const values = [
+    { metric: 'ema_50'},
+    { metric: 'ema_250'},
+    { metric: 'mom_50'},
+    { metric: 'mom_250'},
+    { metric: 'rsi_50'},
+    { metric: "rsi_250"},
+    { metric: "open"},
+    { metric: "high"},
+    { metric: "low"},
+    { metric: "close"},
+    { metric: "volume"}
+  ];
+
+function ComboBox() {
+
+    return (
+      <Autocomplete
+        id="combo-box-demo"
+        options={values}
+        getOptionLabel={(option) => option.metric}
+        style={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+      />
+    );
+  }
+
+export default function SearchPage() {
+  const classes = useStyles();
+
+  return(
+    <div className="root">
+      <NavBar></NavBar>
+      <main className={classes.content}>
+        <h1 className="Header">SearchPage</h1>
+        <div className = "SearchBox">
+            <ComboBox></ComboBox>
+            <div className="SearchGraph">
+                <Graph legendPosition="bottom" displayTitle={true} displayLegend = {false} metrics = "ema_50"/>    
             </div>
-        )
-    }
+      </div>
+      </main>
+    </div>
+)
+
+
 }
 
-export default SearchPage;
+// class SearchPage extends Component { 
+
+//     render() {
+//         return(
+//             <div className = "root">
+//               <NavBar></NavBar>
+//               <main className = "content">
+//                 <h1 className="Header">SearchPage</h1>
+//                 <div className = "SearchBox">
+//                     <ComboBox></ComboBox>
+//                     <div className="SearchGraph">
+//                         <Graph legendPosition="bottom" displayTitle={true} displayLegend = {false} metrics = "ema_50"/>    
+//                     </div>
+//               </div>
+//               </main>
+//             </div>
+//         )
+//     }
+// }
+
+// export default SearchPage;
